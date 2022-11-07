85827de235e7df55030dc317dedbc48dd6913fec
Viktor Romanyuk <v.romanyuk@softonix.org>
2022-11-07 18:50:39 +0200

    renamed pages to views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routeNames } from './routeNames'
 
-import Contacts from '@/pages/Contacts.vue'
-import UpsertContact from '@/pages/UpsertContact.vue'
+import Contacts from '@/views/Contacts.vue'
+import UpsertContact from '@/views/UpsertContact.vue'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
